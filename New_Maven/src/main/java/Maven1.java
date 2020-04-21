6ce3ff82459c260c7ad3da9fc1b1fb911bc51285
roselin <roselin@localhost>
2020-04-21 20:59:42 +0530

    NewUpdateMavenBuild

---
+++ New_Maven/src/main/java/Maven1.java
@@ -0,0 +1,16 @@
+
+public class Maven1 {
+public class A
+{
+	public void r()
+	{
+		System.out.print("Welcome");
+	}
+}
+public static void main(String args[])
+{
+	Maven1 m=new Maven1();
+	A a=m.new A();
+	a.r();
+}
+}
